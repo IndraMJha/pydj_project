d0718155376cf297db9d9feffb771052709e74bb
INDRA JHA <indramohan29@gmail.com>
2026-03-10 00:48:47 +0530

    Fix formatting issues in manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+"""Django's command-line utility for administrative tasks."""
 
 def main():
     """Run administrative tasks."""
