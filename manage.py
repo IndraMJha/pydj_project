fc96b271ec5524a48df65140e613caa76fb10e45
INDRA JHA <indramohan29@gmail.com>
2026-03-10 00:53:09 +0530

    Remove duplicate docstring in manage.py
    Removed duplicate docstring for manage.py.

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-"""Django's command-line utility for administrative tasks."""
 
 def main():
     """Run administrative tasks."""
